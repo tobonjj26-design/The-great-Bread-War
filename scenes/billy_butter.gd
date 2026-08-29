19b6783c6166818182f885e2717a0ef556e1fe09
tobonjj26-design <tobonjj26@gmail.com>
2026-08-29 15:12:55 -0500

    more

--- scenes/billy_butter.gd
+++ scenes/billy_butter.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-const SPEED = 20.0
+
 const CHARGE_SPEED = 70.0
 const CHARGE_RANGE_X = 60.0
 const CHARGE_RANGE_Y = 10.0
@@ -12,10 +12,6 @@ const KNOCKBACK_SPEED = 90.0
 const KNOCKBACK_UP = -120.0
 
 var direction = 1
-var turn_cooldown = 0.0
-var no_floor_frames = 0
-const NO_FLOOR_THRESHOLD = 3
-
 var health = MAX_HEALTH
 var is_charging = false
 var invincible_timer = 0.0
@@ -26,8 +22,6 @@ var entered_phase2 = false
 @export var enemy_to_summon: PackedScene
 
 @onready var sprite = $AnimatedSprite2D
-@onready var wall_ray = $WallRayCast
-@onready var tilemap = get_node("/root/Game/TileMapLayer")
 
 func _ready() -> void:
 	sprite.animation_finished.connect(_on_sprite_animation_finished)
@@ -49,7 +43,6 @@ func _physics_process(delta: float) -> void:
 
 	var jammy = get_tree().get_first_node_in_group("player")
 
-	# --- Entrar a FASE 2 (última vida): reproduce "transition" una sola vez ---
 	if health <= 1 and not entered_phase2:
 		entered_phase2 = true
 		sprite.play("transition")
@@ -67,26 +60,13 @@ func _physics_process(delta: float) -> void:
 			is_charging = true
 			direction = sign(diff.x) if diff.x != 0 else direction
 
-	wall_ray.target_position.x = 8 * direction
-
-	if not has_floor_ahead():
-		no_floor_frames += 1
+	if is_charging:
+		velocity.x = direction * CHARGE_SPEED
 	else:
-		no_floor_frames = 0
+		velocity.x = 0
 
-	if turn_cooldown > 0:
-		turn_cooldown -= delta
-	elif not is_charging and (wall_ray.is_colliding() or no_floor_frames >= NO_FLOOR_THRESHOLD):
-		direction *= -1
-		turn_cooldown = 0.3
-		no_floor_frames = 0
-
-	var current_speed = CHARGE_SPEED if is_charging else SPEED
-	velocity.x = direction * current_speed
 	sprite.flip_h = direction < 0
 
-	# --- Animaciones ---
-	# En fase 2, la animación la controla la transición (transition -> Move), no se toca aquí.
 	if not entered_phase2:
 		if is_charging:
 			sprite.play("Run")
@@ -98,12 +78,6 @@ func _physics_process(delta: float) -> void:
 func _on_sprite_animation_finished() -> void:
 	if sprite.animation == "transition":
 		sprite.play("Move")
-
-func has_floor_ahead() -> bool:
-	var check_pos = global_position + Vector2(6 * direction, 12)
-	var local_pos = tilemap.to_local(check_pos)
-	var map_pos = tilemap.local_to_map(local_pos)
-	return tilemap.get_cell_source_id(map_pos) != -1
 
 func summon_enemy() -> void:
 	var enemy = enemy_to_summon.instantiate()
