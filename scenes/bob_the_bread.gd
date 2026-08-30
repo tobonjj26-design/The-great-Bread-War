de1ad78ad5850e27709c5ecfd86a36c3e3c939b9
tobonjj26-design <tobonjj26@gmail.com>
2026-08-30 18:13:20 -0500

    bread

--- scenes/bob_the_bread.gd
+++ scenes/bob_the_bread.gd
@@ -4,3 +4,7 @@ func _on_win_area_body_entered(body: Node2D) -> void:
 	if body.name == "Jammy":
 		get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
  
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
